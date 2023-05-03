d6c17aca7753a63358140dd32335aed2c4d7978a
Vihitha-mohan <84505072+Vihitha-mohan@users.noreply.github.com>
2023-05-03 14:31:11 +0400

    Update updateSettings.js

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -16,6 +16,7 @@ export const updateSettings = async(data,type)=>
 if(res.data.status === 'success')
 {
     showAlert('success',`${type.toUpperCase()} updated successfully`);
+    window.location.reload(true);
 }
 
     }catch(err)
